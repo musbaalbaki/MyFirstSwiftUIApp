57cd812d21c6a3c4e302f0b591cbad259bb0c024
mustafa <mustafa.baalbaki@gmail.com>
2020-09-05 16:06:32 +0900

    Initial Commit

---
+++ Shared/MyFirstSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstSwiftUIAppApp.swift
+//  Shared
+//
+//  Created by Mustafa Baalbaki on 9/5/20.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
